9ff348c57274ee646119f4b27a0ec728adf0b800
ScottKrysa <32242896+ScottKrysa@users.noreply.github.com>
2018-06-29 13:09:38 -0400

    Update enron.py

--- enron.py
+++ enron.py
@@ -1,6 +1,8 @@
 import os
 from email.parser import Parser
 
+import csv
+
 """
 Mark the folder containing all of the emails
 """
@@ -18,12 +20,45 @@ Using Parser to open the emails
 
 raw_file_dir = "/Users/Scott/Desktop/tensorflow_projects/maildir/lay-k/all_documents/1."
 
-with open(raw_file_dir, "r") as f:
-	data = f.read()
+# with open(raw_file_dir, "r") as f:
+# 	data = f.read()
 
-email = Parser().parsestr(data)
+# email = Parser().parsestr(data)
 
-print("\n To: " , email['to'])
-print("\n From: " , email['from'])
-print("\n Subject: " , email['subject'])
-print("\n \n Body: " , email.get_payload())
+# print("\n To: " , email['to'])
+# print("\n From: " , email['from'])
+# print("\n Subject: " , email['subject'])
+# print("\n \n Body: " , email.get_payload())
+
+to_email_list = []
+from_email_list = []
+email_body = []
+
+def email_analyse(inputfile, to_email_list, from_email_list, email_body):
+    with open(inputfile, "r") as f:
+        data = f.read()
+ 
+    email = Parser().parsestr(data)
+ 
+    to_email_list.append(email['to'])
+    from_email_list.append(email['from'])
+ 
+    email_body.append(email.get_payload())
+
+email_analyse(raw_file_dir, to_email_list, from_email_list, email_body)
+
+"""
+Writing a CSV file and testing writing data to it
+"""
+
+master_email_list = [['name_of_file', 'to_email_list', 'from_email_list', 'email_body'], \
+			[raw_file_dir, to_email_list, from_email_list, email_body]]
+
+f = open('enron_test.csv', 'w')
+
+with f:
+
+    writer = csv.writer(f)
+    
+    for row in master_email_list:
+        writer.writerow(row)
