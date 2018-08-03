40d437f97848caecf5dba0c1fa9e32c8390d3861
ScottKrysa <32242896+ScottKrysa@users.noreply.github.com>
2018-08-03 10:36:16 -0400

    Update enron.py
    Changed my approach and started populating a Pandas database from lists instead of working with JSON.

--- enron.py
+++ enron.py
@@ -1,64 +1,101 @@
 import os
-from email.parser import Parser
-
 import csv
+import pandas as pd
+from pandas.io.json import json_normalize
+from email_analyzer_pd import EmailAnalyzer
+from bag_of_words import BagOfWords
+import json
+
+import pprint
+
 
 """
 Mark the folder containing all of the emails
 """
-rootdir = "/Users/Scott/Desktop/tensorflow_projects/maildir"
+
+
+rootdir = "/Users/Scott/Desktop/tensorflow_projects/maildir_test"
+
 
 """
-Loop through all subfolders and count files for each
-"""
-# for directory, subdirectory, filenames in  os.walk(rootdir):
-#     print(directory, subdirectory, len(filenames))
-
-"""
-Using Parser to open the emails
+Loop through all subfolders and get the filenames for each
 """
 
-raw_file_dir = "/Users/Scott/Desktop/tensorflow_projects/maildir/lay-k/all_documents/1."
 
-# with open(raw_file_dir, "r") as f:
-# 	data = f.read()
+email_filenames = []
 
-# email = Parser().parsestr(data)
+for dirpath, subdirs, files in os.walk(rootdir):
+	for file in files:
+		if not file.startswith('.'):     # Ignore hidden files
+			email_filenames.append(os.path.join(dirpath, file))
 
-# print("\n To: " , email['to'])
-# print("\n From: " , email['from'])
-# print("\n Subject: " , email['subject'])
-# print("\n \n Body: " , email.get_payload())
-
-to_email_list = []
-from_email_list = []
-email_body = []
-
-def email_analyse(inputfile, to_email_list, from_email_list, email_body):
-    with open(inputfile, "r") as f:
-        data = f.read()
- 
-    email = Parser().parsestr(data)
- 
-    to_email_list.append(email['to'])
-    from_email_list.append(email['from'])
- 
-    email_body.append(email.get_payload())
-
-email_analyse(raw_file_dir, to_email_list, from_email_list, email_body)
 
 """
-Writing a CSV file and testing writing data to it
+Loop through through the files and create a 
+Pandas Database using EmailAnalyzer()
 """
 
-master_email_list = [['name_of_file', 'to_email_list', 'from_email_list', 'email_body'], \
-			[raw_file_dir, to_email_list, from_email_list, email_body]]
 
-f = open('enron_test.csv', 'w')
+email_list = []
 
-with f:
+try:
+	for filename in email_filenames:
+		with open(filename, "r") as f:
+			data = f.read() 
+		df_tmp = EmailAnalyzer().parse(data)
+		email_list.append(df_tmp)
+except:
+	pass
 
-    writer = csv.writer(f)
-    
-    for row in master_email_list:
-        writer.writerow(row)
+email_df = pd.DataFrame(email_list)
+
+email_df.columns=['from','to','date','body']
+
+
+"""
+Converting dates from lists to raw datetimes
+"""
+
+
+datetime_list = []
+for x in range(len(email_df)):
+	datetime_obj = email_df['date'][x][0]
+	datetime_list.append(datetime_obj)
+
+email_df['datetime'] = datetime_list
+email_df['datetime'] = pd.to_datetime(email_df['datetime'], utc=True)
+email_df = email_df.set_index('datetime')
+email_df = email_df.drop(['date'], axis=1)
+email_df.sort_index(inplace=True)
+
+
+pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(email_df)
+
+
+"""
+Testing BagOfWords on a Sample Email Body
+"""
+
+
+# token_sample = BagOfWords().get_tokens(email_df['body'][0][0])
+
+# pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(token_sample)
+# print(list(token_sample))
+
+
+"""
+Iterate through the files and count words in body
+"""
+
+
+token_list = []
+
+for x in range(len(email_df)):
+	tokens = BagOfWords().get_tokens(email_df['body'][x][0])
+	token_list.append(tokens)
+
+email_df['body_tokens'] = token_list
+
+pp.pprint(email_df)
