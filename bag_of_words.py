a714d92c81500fb28d41aa1dcc70af09cdd29ac9
ScottKrysa <32242896+ScottKrysa@users.noreply.github.com>
2018-08-03 10:37:59 -0400

    Update bag_of_words.py

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,5 +1,7 @@
 import nltk
-from nltk import word_tokenize
+# from nltk import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 from collections import Counter
 
 class BagOfWords:
@@ -8,7 +10,27 @@ class BagOfWords:
 		self.data = {}
 
 	def get_tokens(self, email_body):
-		tokens = word_tokenize(email_body)
-		top_10 = Counter(tokens).most_common(10)
+		stop_words = set(stopwords.words('english'))
+		tokenizer = RegexpTokenizer(r'\w+')
+		raw_tokens = tokenizer.tokenize(email_body)
+		# raw_tokens = word_tokenize(email_body)
+		# filtered_tokens = filter(lambda token: token not in stop_words, raw_tokens)
+		filtered_tokens = [w for w in raw_tokens if not w in stop_words]
+		
+		for word in filtered_tokens:
+			if len(word) < 4:
+				filtered_tokens.remove(word)
 
-		return top_10
+		# top_10 = Counter(filtered_tokens).most_common(10)
+		# self.data = top_10
+		self.data = list(filtered_tokens)
+		return self.data
+	
+"""
+Not sure if the following works. It's more of a placeholder for the moment.
+"""
+	def top_ten(self, tokens):
+		top_10 = Counter(tokens).most_common(10)
+		self.data = top_10
+		return self.data
+	
