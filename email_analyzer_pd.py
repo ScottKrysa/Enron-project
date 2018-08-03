65baf72c8538c7d6926025a5ada7c6a62538facf
ScottKrysa <32242896+ScottKrysa@users.noreply.github.com>
2018-08-03 10:40:54 -0400

    Add files via upload

---
+++ email_analyzer_pd.py
@@ -0,0 +1,57 @@
+import email.utils
+from email.parser import Parser
+import pandas as pd
+from datetime import datetime
+
+import json
+
+class EmailAnalyzer:
+	def __init__(self):
+		self.data = {}
+
+	def parse(self, email):
+		
+		email = Parser().parsestr(email)
+    
+	    ## If statement to catch variations in the TO, including restricted enron lists
+		# tmp_to_email_list = []
+		to_email_list = []
+		from_email_list = []
+		email_body = []
+		date_time = []
+
+		if email['to']:
+			email_to = email['to']
+			email_to = email_to.replace("\n", "")
+			email_to = email_to.replace("\t", "")
+			email_to = email_to.replace(" ", "")
+			email_to = email_to.split(",")
+			for email_to_1 in email_to:
+				to_email_list.append(email_to_1) 
+			# to_email_list.append(tmp_to_email_list)
+		else:
+			to_email_list.append(None)
+	
+		## Appending the FROM
+		from_email_list.append(email['from'])
+
+		## Appending the BODY
+		email_body.append(email.get_payload().replace("\n", " "))
+		# print(email_body)
+
+		## Appending the DATETIME
+		# date_time.append(email['date'])
+		datetime_obj = datetime.strptime(email['date'][:-6], '%a, %d %b %Y %H:%M:%S %z')
+		date_time.append(datetime_obj)
+		# date_time.append(format_datetime(email['date']))
+
+		self.data = [
+						from_email_list, # email['from'],
+						to_email_list,
+						date_time, # email['date'],
+						email_body
+						]
+
+		return self.data
+
+
